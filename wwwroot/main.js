325f1912fbe07f693f68d4e6400d36b2b45e92f3
Max <gang121191@gmail.com>
2023-03-29 01:47:30 +0300

    worked on client

--- wwwroot/main.js
+++ wwwroot/main.js
@@ -1,29 +1,44 @@
-
+const container = document.getElementById('table-container');
 // Обирається таблиця
 const tableSelect = document.getElementById('table-select');
 
-tableSelect.addEventListener('change', (event) => {
+tableSelect.addEventListener('change', async (event) => {
 
     const selectedTable = event.target.value;
-  
-    console.log(`Selected table: ${selectedTable}`);
-  });
+    
+    await drawTable(`/api/${selectedTable}`);
+
+    initDeleteButtons(selectedTable);
+
+    // await drawTable(`/api/${selectedTable}`).then(html => {
+    //     const container = document.getElementById('table-container');
+    //     container.innerHTML = html;
+    // });
+});
 
 
 // Виводиться таблиця в html вигляді
-async function generateTableHtml(url, columnNames){
+async function drawTable(url){
     const response = await fetch(url);
-    const data = await response.json();
+    let data;
+    if(response.ok === true)
+    {
+        data = await response.json();
+    }
 
     //forming table
     const table = document.createElement('table');
 
     const headerRow = document.createElement('tr');
+
+    columnNames = await getTableHeader(url);
     columnNames.forEach(columnName => {
         const headerCell  = document.createElement('th');
         headerCell.textContent = columnName;
         headerRow.appendChild(headerCell);
     });
+    const emptyHeaderCell  = document.createElement('th');
+    headerRow.appendChild(emptyHeaderCell);
     table.appendChild(headerRow);
 
     data.forEach(item => {
@@ -33,15 +48,47 @@ async function generateTableHtml(url, columnNames){
             cell.textContent = value;
             row.appendChild(cell);
         });
+
+        const cell = document.createElement('td');
+        const deleteButton = document.createElement('button');
+        deleteButton.id = "delete";
+        deleteButton.setAttribute('data-id', Object.values(item)[0]);
+        deleteButton.textContent = 'Delete';
+
+        cell.appendChild(deleteButton);
+        row.appendChild(cell);
         table.appendChild(row);
     });
-
-    return table.outerHTML;
+    container.innerHTML = table.outerHTML;
 }
 
+async function getTableHeader(url)
+{
+    const response = await fetch(url+'/header');
+    return await response.json();
+}
 
-const table1 = ['Код клієнта', 'Назва Клієнта'];
-generateTableHtml('/api/Clients', table1).then(html => {
-    const container = document.getElementById('table-container');
-    container.innerHTML = html;
-});
+function initDeleteButtons(selectedTable){
+    const deleteButtons = document.querySelectorAll('#delete');
+
+    deleteButtons.forEach((button) => {
+        button.addEventListener('click', async () => {
+            const id = button.dataset.id;
+
+            console.log(`Delete button clicked for item ${id}`);
+            await deleteRow(selectedTable, id);
+        });
+    });
+}
+
+async function deleteRow(table, id)
+{
+    const response = await fetch(`/api/${table}/${id}`, 
+    {
+        method: 'DELETE',
+    })
+    if(response.ok === true)
+    {
+        await drawTable(`/api/${table}`);
+    }
+}
